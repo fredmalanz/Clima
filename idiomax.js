5456a6a220896988b848252ff0e69cab55509d50
fredmalanz <fredmalanz98@gmail.com>
2026-04-23 19:55:42 -0600

    Actualizacion autocomplete

---
+++ idiomax.js
@@ -0,0 +1,27 @@
+const idiomaActual = document.getElementById('idioma');
+const listaIdiomas = document.getElementById('idiomas');
+const idiomas = document.getElementsByClassName('opcion');
+
+//info
+const humedad = document.getElementsByClassName('humidity');
+
+idiomaActual.addEventListener('click',()=>{
+    listaIdiomas.classList.toggle('toggle');
+});
+
+const opcionesArray = Array.from(idiomas);
+
+opcionesArray.forEach((opcion)=>{
+    opcion.addEventListener('click',()=>{
+        const idioma = opcion.getElementsByTagName('span')[0].textContent.toLowerCase();
+        console.log(idioma);
+        establecerIdioma(idioma);
+    });
+})
+
+function establecerIdioma(idioma) {
+    idiomaActual.getElementsByTagName('img')[0].src = `Banderas/${idioma}.png`;
+    
+}
+
+
